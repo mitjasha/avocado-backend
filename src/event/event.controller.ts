ba093c2184fabe2b546929e60d3e9b7fa853a2fb
mitjasha <mitjasha@gmail.com>
2023-02-09 02:59:03 +0300

    feat: get events

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Param, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
+import { GetUser } from "src/auth/get-user.decorator";
 import { AuthGuard } from "src/auth/guards/auth.guard";
+import { UserEntity } from "src/auth/user.entity";
 import { CreateEventDto } from "./dto/createEvent.dto";
 import { EventEntity } from "./event.entity";
 import { EventService } from "./event.service";
@@ -8,7 +10,7 @@ import { EventService } from "./event.service";
 export class EventController {
   constructor(private eventService: EventService) {}
 
-  @Post(":userID/add")
+  @Post(":userID/addEvent")
   @UseGuards(AuthGuard)
   async createEvent(
     @Param("userID") userID: string,
@@ -17,5 +19,11 @@ export class EventController {
   ): Promise<EventEntity> {
     const child = await this.eventService.getUser(userID);
     return this.eventService.createEvent(createEventDto, child);
+  }
+
+  @Get(":userID/getAllEvents")
+  @UseGuards(AuthGuard)
+  async getEvents(@GetUser() user: UserEntity): Promise<EventEntity[]> {
+    return this.eventService.getEvents(user);
   }
 }
