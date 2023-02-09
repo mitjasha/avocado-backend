95b41d630650e9fced3fff5bf6ed0371529633ee
mitjasha <mitjasha@gmail.com>
2023-02-10 01:19:24 +0300

    feat: add event activity crud

--- src/event-activity/event-activity.controller.ts
+++ src/event-activity/event-activity.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { GetUser } from "src/auth/get-user.decorator";
+import { UserEntity } from "src/auth/user.entity";
+import { CreateEventActivityDto } from "./dto/createEventActivity.dto";
+import { UpdateEventActivityDto } from "./dto/updateEventActivity.dto";
+import { EventActivityEntity } from "./event-activity.entity";
+import { EventActivityService } from "./event-activity.service";
 
-@Controller('event-activity')
-export class EventActivityController {}
+@Controller("event-activity")
+export class EventActivityController {
+  constructor(private eventService: EventActivityService) {}
+
+  @Post(":userID/addEvent")
+  @UseGuards(AuthGuard)
+  async createEvent(
+    @Param("userID") userID: string,
+    @Body()
+    createEventDto: CreateEventActivityDto,
+  ): Promise<EventActivityEntity> {
+    const child = await this.eventService.getUser(userID);
+    return this.eventService.createEvent(createEventDto, child);
+  }
+
+  @Get(":userID/getAllEvents")
+  @UseGuards(AuthGuard)
+  async getEvents(@GetUser() user: UserEntity): Promise<EventActivityEntity[]> {
+    return this.eventService.getEvents(user);
+  }
+
+  @Put("/:id")
+  @UseGuards(AuthGuard)
+  async updateEvent(
+    @Param("id") id: string,
+    @Body() updateEventDto: UpdateEventActivityDto,
+  ) {
+    return this.eventService.updateEvent(id, updateEventDto);
+  }
+
+  @Delete("/:id")
+  @UseGuards(AuthGuard)
+  async deleteEvent(@Param("id") id: string) {
+    return this.eventService.deleteEvent(id);
+  }
+}
