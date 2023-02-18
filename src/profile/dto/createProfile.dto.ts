f7b215d07e54bd07917000f059316d41317dff24
mitjasha <mitjasha@gmail.com>
2023-02-19 02:38:05 +0300

    feat: del IsNotEmpty

--- src/profile/dto/createProfile.dto.ts
+++ src/profile/dto/createProfile.dto.ts
@@ -3,31 +3,22 @@ import { EGender } from "../profile-gender.enum";
 import { EGoal } from "../profile-goal.enum";
 
 export class CreateProfileDto {
-  @IsNotEmpty()
   firstName: string;
 
-  @IsNotEmpty()
   lastName: string;
 
-  @IsNotEmpty()
   @IsEnum(EGender)
   gender: EGender;
 
-  @IsNotEmpty()
   @IsDateString()
   birth: string;
-
-  @IsNotEmpty()
   weight: string;
 
-  @IsNotEmpty()
   height: number;
 
-  @IsNotEmpty()
   @IsEnum(EGoal)
   goal: EGoal;
 
-  @IsNotEmpty()
   targetWeight: string;
 
   photo: string;
